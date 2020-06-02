f36db09f62171ba42bd7703f72e41eaac1d967b3
Gustavo Silva <gstvds@icloud.com>
2020-06-02 12:58:53 -0300

    feat: Adds routes to get items and to create points, connecting points to items

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,9 +1,49 @@
 import express from 'express';
 
+import knex from './database/connection';
+
 const routes = express.Router();
 
-routes.get('/', (req, res) => {
-  res.send({ message: 'Hello World' });
+routes.get('/items', async (request, response) => {
+  const items = await knex('items').select('*');
+
+  const serializedItems = items.map((item) => {
+    return {
+      id: item.id,
+      title: item.title,
+      image_url: `http://localhost:3333/uploads/${item.image}`,
+    }
+  });
+
+  return response.send(serializedItems);
+});
+
+routes.post('/points', async (request, response) => {
+  const { name, email, whatsapp, latitude, longitude, city, uf, items } = request.body;
+
+  // trx extends for 'transacation'. it means that, if the first query fails, the second one will not be executed
+  const trx = await knex.transaction();
+
+  const insertedIds = await trx('points').insert({
+    image: 'image_fake',
+    name,
+    email,
+    whatsapp,
+    latitude,
+    longitude,
+    city,
+    uf,
+  });
+
+  const point_id = insertedIds[0];
+
+  const pointItems = items.map((item_id: number) => {
+    return { item_id, point_id };
+  })
+
+  await trx('point_items').insert(pointItems);
+
+  return response.send({ success: true });
 });
 
 export default routes;
