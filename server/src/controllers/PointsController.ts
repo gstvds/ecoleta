a128bec2b4b274afb9e039b1b5c7ff39d036c973
Gustavo Silva <gstvds@icloud.com>
2020-06-02 15:59:16 -0300

    feat: Adds index and show methods to PointsController

--- server/src/controllers/PointsController.ts
+++ server/src/controllers/PointsController.ts
@@ -2,6 +2,23 @@ import { Request, Response } from 'express';
 import knex from '../database/connection';
 
 class PointsController {
+  async index(request: Request, response: Response) {
+    const { city, uf, items } = request.query;
+
+    const parsedItems = String(items)
+      .split(',')
+      .map(item => Number(item.trim()));
+
+    const points = await knex('points')
+      .join('point_items', 'points.id', '=', 'point_items.point_id')
+      .whereIn('point_items.item_id', parsedItems)
+      .where('city', String(city))
+      .where('uf', String(uf))
+      .distinct()
+      .select('points.*');
+
+    return response.json(points);
+  }
   async create(request: Request, response: Response) {
     const { name, email, whatsapp, latitude, longitude, city, uf, items } = request.body;
   
@@ -19,8 +36,29 @@ class PointsController {
     })
   
     await trx('point_items').insert(pointItems);
+    await trx.commit();
   
     return response.send({ id: point_id, ...point });
+  };
+
+  async show(request: Request, response: Response) {
+    const { id } = request.params;
+
+    const point = await knex('points').where('id', id).first();
+
+    if (!point) {
+      return response.status(400).json({
+        name: 'Point not found',
+        message: 'The point you are trying to see does not exists'
+      });
+    }
+
+    const items = await knex('items')
+      .join('point_items', 'items.id', '=', 'point_items.item_id')
+      .where('point_items.point_id', id)
+      .select('items.title');
+
+    return response.json({ point, items });
   }
 };
 
--- server/src/routes.ts
+++ server/src/routes.ts
@@ -9,7 +9,10 @@ const pointsController = new PointsController();
 const itemsController = new ItemsController();
 
 routes.get('/items', itemsController.index);
+
 routes.post('/points', pointsController.create);
+routes.get('/points', pointsController.index);
+routes.get('/points/:id', pointsController.show);
 
 
 export default routes;
