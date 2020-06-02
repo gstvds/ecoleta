ed311c54435f50b941e5c3edad7af6984001312f
Gustavo Silva <gstvds@icloud.com>
2020-06-02 12:37:27 -0300

    feat: Adds seeds and images and create a route to obtain images from the server

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,12 @@
 import express from 'express';
+import path from 'path';
 import routes from './routes';
 
 const app = express();
 app.use(express.json());
 app.use(routes);
+
+// express.static is used to declare static files from a specific folder
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 
 app.listen(3333);
