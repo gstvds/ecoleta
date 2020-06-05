39ed5936fbb85a93417e21ce571ec4fdc756fe69
Gustavo Silva <gstvds@icloud.com>
2020-06-05 16:06:56 -0300

    feat: Adds image upload to the database

--- server/src/controllers/PointsController.ts
+++ server/src/controllers/PointsController.ts
@@ -17,28 +17,37 @@ class PointsController {
       .distinct()
       .select('points.*');
 
-    return response.json(points);
+      const serializedPoints = points.map((point) => {
+        return {
+          ...point,
+          image_url: `http://192.168.0.4:3333/uploads/${point.image}`,
+        }
+      });
+
+    return response.json(serializedPoints);
   }
   async create(request: Request, response: Response) {
     const { name, email, whatsapp, latitude, longitude, city, uf, items } = request.body;
-  
+
     // trx extends for 'transacation'. it means that, if the first query fails, the second one will not be executed
     const trx = await knex.transaction();
-  
-    const image = 'https://images.unsplash.com/photo-1583258292688-d0213dc5a3a8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60';
-    const point = { image, name, email, whatsapp, latitude, longitude, city, uf };
+
+    const point = { image: request.file.filename, name, email, whatsapp, latitude, longitude, city, uf };
 
     const insertedIds = await trx('points').insert(point);
-  
+
     const point_id = insertedIds[0];
-  
-    const pointItems = items.map((item_id: number) => {
-      return { item_id, point_id };
-    })
-  
+
+    const pointItems = items
+      .split(',')
+      .map((item: string) => Number(item.trim()))
+      .map((item_id: number) => {
+        return { item_id, point_id };
+      });
+
     await trx('point_items').insert(pointItems);
     await trx.commit();
-  
+
     return response.send({ id: point_id, ...point });
   };
 
@@ -54,12 +63,17 @@ class PointsController {
       });
     }
 
+    const serializedPoint = {
+      ...point,
+      image_url: `http://192.168.0.4:3333/uploads/${point.image}`,
+    };
+
     const items = await knex('items')
       .join('point_items', 'items.id', '=', 'point_items.item_id')
       .where('point_items.point_id', id)
       .select('items.title');
 
-    return response.json({ point, items });
+    return response.json({ serializedPoint, items });
   }
 };
 
--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,18 +1,21 @@
 import express from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
 import PointsController from './controllers/PointsController';
 import ItemsController from './controllers/ItemsController';
 
 // controller patterns: index => list of items, show => single item, create, update, delete.
 const routes = express.Router();
+const upload = multer(multerConfig);
+
 const pointsController = new PointsController();
 const itemsController = new ItemsController();
 
 routes.get('/items', itemsController.index);
 
-routes.post('/points', pointsController.create);
+routes.post('/points', upload.single('image'), pointsController.create);
 routes.get('/points', pointsController.index);
 routes.get('/points/:id', pointsController.show);
-
 
 export default routes;
